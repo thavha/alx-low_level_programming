dd97f8e02871d6da38daf109e575412531fde8bf
thavha <thavha.tsiwana@gmail.com>
2022-03-10 13:04:42 -0800

    task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  *  main Entry point This function just printd words
- *  Description: This function just print words 
+ *
  *  Return: Always 0 (Success)
  */
 int main(void)
