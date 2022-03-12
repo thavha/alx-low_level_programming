f8813e6173ea2f9fde8406e2a5ad859e306667aa
thavha <thavha.tsiwana@gmail.com>
2022-03-12 00:00:24 -0800

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 0;
+
+	for (ch; ch <= 9; ch++)
+	{
+	printf("%d", ch);
+	}
+	printf('\n');
+	return (0);
+}
