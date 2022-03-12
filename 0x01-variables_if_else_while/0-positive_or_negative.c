4c645b86f1849abcf2552098fb788af38d3e6a17
thavha <thavha.tsiwana@gmail.com>
2022-03-11 22:06:16 -0800

    redo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,24 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *  main - Entry point This function just printd words
+ *  This function just printd words
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	int l;
-	char r[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l = n % 10;
-
-	if (l>5)
-        	printf("%s %d is %d and is greater than 5\n",r,n,l);
-    	else if (l==0)
-        	printf("%s %d is %d and is 0\n",r,n,l);
-    	else if (l<6)
-        	printf("%s %d is %d and is less than 6 and not 0\n",r,n,l);
-	
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int l;
+	char r[] = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+
+	if (l>5)
+        	printf("%s %d is %d and is greater than 5\n",r,n,l);
+    	else if (l==0)
+        	printf("%s %d is %d and is 0\n",r,n,l);
+    	else if (l<6)
+        	printf("%s %d is %d and is less than 6 and not 0\n",r,n,l);
+	
+	return (0);
+}
