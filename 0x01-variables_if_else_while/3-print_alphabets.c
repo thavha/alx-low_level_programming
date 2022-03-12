8b5fbd51e1288570c75a21926dcfe12fc9e9ccc1
thavha <thavha.tsiwana@gmail.com>
2022-03-11 23:22:59 -0800

    task 5

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,10 +6,15 @@
  */
 int main(void)
 {
-	char ch;
+ 	char ch;
+	char up;
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 	putchar(ch);
+	}
+	for (up = 'A'; up <= 'Z'; up++)
+	{
+	putchar(up);
 	}
 	putchar("\n");
 	return (0);
