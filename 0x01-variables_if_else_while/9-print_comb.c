f432c28c610c00e528f850f4447b9b6298be518c
thavha <thavha.tsiwana@gmail.com>
2022-03-12 02:57:07 -0800

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,9 +8,13 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 0; ch <= 9; ch++)
-	printf("%d", ch);
-	printf("\n");
-
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+		if (ch !='9')
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
 	return (0);
 }
