33c63ad747907a1c8ff47a677a77643ca84659a0
thavha <thavha.tsiwana@gmail.com>
2022-03-16 12:06:10 -0700

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -  prints alphabets in lower case, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int print_alphabet(void)
+{
+	char al;
+
+	for (al = 'a'; a <= 'z'; al++)
+	{
+		_putchar(al);
+	}
+	_putchar('\n');
+
+	return (0);
+}
