1502698380d7b92597b97a42e4a3a6f3f0d2ddf8
thavha <thavha.tsiwana@gmail.com>
2022-03-16 13:17:28 -0700

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign -  prints alphabets in lower case, followed by a new line
+ * Return: Always 0 (Success)
+ * @n: - is the input alphabet
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-')
+		return (-1);
+	}
+	return (0);
+}
