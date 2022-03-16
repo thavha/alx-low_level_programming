54497fdca3f00ecfdd0ae20af395227e10505c9e
thavha <thavha.tsiwana@gmail.com>
2022-03-16 12:22:12 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  prints alphabets x10 in lower case, followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char al;
+	int c = 0;
+
+	while (c < 10)
+	{
+		for (al = 'a'; al <= 'z'; al++)
+		{
+			_putchar(al);
+		}
+		c++;
+	}
+	_putchar('\n');
+}
