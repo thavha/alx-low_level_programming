b3881c03d01c19434917fa2d45565e957ae72615
thavha <thavha.tsiwana@gmail.com>
2022-03-16 13:51:38 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs -  prints the abs
+ * Return: Always 0 (Success)
+ * @n: - is the input alphabet
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
