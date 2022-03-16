4eea7a113ce04e0bf0aaf3a9e1350a99ae110d50
thavha <thavha.tsiwana@gmail.com>
2022-03-16 14:55:08 -0700

    trying 11

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,8 @@
 /**
  * add - adds 2 numbers
  * Return: Always 0 (Success)
- * @a - first number
- * @b - second numner
+ * @a: - first number
+ * @b: - second numner
  */
 int add(int a, int b)
 {
