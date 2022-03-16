e3da6e630d34c01cd767b10eed973ea87b46098a
thavha <thavha.tsiwana@gmail.com>
2022-03-16 14:02:15 -0700

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit -  prints the last digit
+ * Return: Always 0 (Success)
+ * @n: - is the input alphabet
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
