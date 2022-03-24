d1739a0687940df1bbe01be6cd996470effabe72
thavha <42216436+thavha@users.noreply.github.com>
2022-03-24 18:28:15 +0200

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,12 +4,12 @@
  * reverse_array - reverses an array
  * @a: array to be reversed
  * @n: number of elements in the array
- */
+*/
 void reverse_array(int *a, int n)
 {
 	int i, j, tmp;
 
-	j = n -1;
+	j = n - 1;
 
 	for (i = 0; i < n / 2; i++)
 	{
