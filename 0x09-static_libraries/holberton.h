835d4d96b127ebf2454480f0a524ddcaedce07c4
thavha <thavha.tsiwana@gmail.com>
2022-04-01 13:54:11 +0200

    fixed problem

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -10,7 +10,7 @@ int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+int _atoi(void);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* HOLBERTON_H */
